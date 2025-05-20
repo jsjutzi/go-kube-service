0ce8a30d90f36507699ceae4e67fe705de9bb5b1
jsjutzi <jsjutzi@gmail.com>
2025-05-20 11:21:20 -0400

    update makefile & add config

--- api/services/sales/main.go
+++ api/services/sales/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"os/signal"
-	"runtime"
 	"syscall"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/jsjutzi/go-kube-service/foundation/logger"
 )
 
@@ -43,7 +46,68 @@ func run(ctx context.Context, log *logger.Logger) error {
 	// -------------------------------------------------------------------------
 	// GOMAXPROCS
 
-	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout        time.Duration `conf:"default:5s"`
+			WriteTimeout       time.Duration `conf:"default:10s"`
+			IdleTimeout        time.Duration `conf:"default:120s"`
+			ShutdownTimeout    time.Duration `conf:"default:20s"`
+			APIHost            string        `conf:"default:0.0.0.0:3000"`
+			DebugHost          string        `conf:"default:0.0.0.0:3010"`
+			CORSAllowedOrigins []string      `conf:"default:*,mask"`
+		}
+		Auth struct {
+			Host string `conf:"default:http://auth-service:6000"`
+		}
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:database-service"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:0"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
+		Tempo struct {
+			Host        string  `conf:"default:tempo:4317"`
+			ServiceName string  `conf:"default:sales"`
+			Probability float64 `conf:"default:0.05"`
+			// Shouldn't use a high Probability value in non-developer systems.
+			// 0.05 should be enough for most systems. Some might want to have
+			// this even lower.
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Sales",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	// -------------------------------------------------------------------------
+	// App Shutting Down
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
@@ -52,6 +116,9 @@ func run(ctx context.Context, log *logger.Logger) error {
 
 	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
 	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	// -------------------------------------------------------------------------
+	// Handle config & build logging
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jsjutzi/go-kube-service
 go 1.24
 
 toolchain go1.24.0
+
+require github.com/ardanlabs/conf/v3 v3.7.2
